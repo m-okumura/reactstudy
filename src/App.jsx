310ea0f7a4c8b1f8542c74b31c8b133d27b1899c
奥村 宗久 <okumuram.gm@gmail.com>
2025-11-19 17:54:55 +0900

    テスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { StrictMode } from 'react';
 // import './App.css';
+import { useState } from 'react';
+import { ColorfullMessage } from './components/ColorfullMessage';
 
 export const App = () => {
-    const onClickButton = () => alert();
-    const contentStyle = {
-        color: "red",
-        fontSize: "20px"
-    };
+  console.log("App");
+  const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(true);
+  const onClickCountup = () => {
+    setNum(num + 1);
+  };
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
   return (
     <>
-        <h1 style={contentStyle}>こんにちは</h1>
-        <p>おげんきですか</p>
-        <button onClick={onClickButton}>ボタン</button>
+      <h1 style={{ color: "red"}}>こんにちは</h1>
+      <ColorfullMessage color="red">お元気ですか</ColorfullMessage>
+      <ColorfullMessage color="green">元気です</ColorfullMessage>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>奥村</p>}
     </>
   );
 };

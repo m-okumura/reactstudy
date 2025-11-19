941f148f40b224bb2ae57b4310ad19ee1df079c7
奥村 宗久 <okumuram.gm@gmail.com>
2025-11-19 14:49:26 +0900

    初めてのコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { StrictMode } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+    const onClickButton = () => alert();
+    const contentStyle = {
+        color: "red",
+        fontSize: "20px"
+    };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1 style={contentStyle}>こんにちは</h1>
+        <p>おげんきですか</p>
+        <button onClick={onClickButton}>ボタン</button>
+    </>
   );
-}
+};
 
 export default App;
